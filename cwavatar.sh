909d6d9b00461fb0c858f6e3747cc8b5a8d5ee0d
cw-tanaka <tanaka@chatwork.co.jp>
2013-06-26 13:36:18 +0900

    Debug

--- cwavatar.sh
+++ cwavatar.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
 orgfile=$1
+output_file=$2
 
 if [ ! -f ${orgfile} ]; then
   echo "Not found file: ${orgfile}"
   exit 0;
 fi
 
-size_set=`identify profile.png | awk '{print $3}'`
-width=`echo ${size_set} | awk '{print $1}'`
-height=`echo ${size_set} | awk '{print $2}'`
+if [ "${output_file}" = "" ]; then
+  output_file="./output.png"
+fi
+
+size_set=`identify ${orgfile} | awk '{print $3}'`
+width=`echo ${size_set} | awk -F'x' '{print $1}'`
+height=`echo ${size_set} | awk -F'x' '{print $2}'`
 
 if [ ${width} -gt ${height} ]; then
   edge=${height}
@@ -20,4 +25,4 @@ fi
 icon_edge=`expr ${edge} / 3`
 
 convert ./img/chatwork.png -resize ${icon_edge}x${icon_edge} ./img/chatwork.tmp.png
-convert ${orgfile} ./img/chatwork.tmp.png -gravity southwest -composite ./output.png
+convert ${orgfile} ./img/chatwork.tmp.png -gravity southwest -composite ${output_file}
